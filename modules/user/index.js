18c98ba79af970e721d30bbdd7dc61a623794a4e
maurinlenglart <maurin.lenglart@gmail.com>
2021-06-05 11:34:17 -0700

    simple eg to create user

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,12 +1,21 @@
 const user = function (server, options, next) {
-    server.route({
+    server.route([{
         path: '/get_all',
         method: 'GET',
         handler: async (req, h) => {
             const users = await req.getModel('User').findAll();
             return h.response(users);
         }
-    });
+    },
+        {
+            method: 'POST',
+            path: '/',
+            handler: async (req, h) => {
+                const user = await req.getModel('User').build(req.payload.user);
+                return user.save()
+            }
+        },
+    ]);
 };
 
 
