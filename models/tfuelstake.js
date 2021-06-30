7f2f2d884a219869fa227f22e7437149a040866e
maurinlenglart <maurin.lenglart@gmail.com>
2021-06-30 14:49:55 -0700

    integer ! or max doesn't work

--- models/tfuelstake.js
+++ models/tfuelstake.js
@@ -20,7 +20,7 @@ module.exports = function (sequelize, DataTypes) {
                 allowNull: false
             },
             edgeNodeId: {
-                type: DataTypes.STRING,
+                type: DataTypes.INTEGER,
                 allowNull: true
             },
             edgeNodeSummary: {
