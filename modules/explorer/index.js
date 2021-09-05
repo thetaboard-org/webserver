69b52bf2e76509e1b8bc8cb17807b7f8d9057ead
maurinlenglart <maurin.lenglart@gmail.com>
2021-09-05 15:20:52 -0700

    remove tokenId as it doesnt match what is shown on the thetadrop website

--- modules/explorer/index.js
+++ modules/explorer/index.js
@@ -309,7 +309,7 @@ const explorer = function (server, options, next) {
                         const nft_metadata_api = await fetch(token_uri)
                         const nft_metadata = await nft_metadata_api.json();
                         obj['url'] = nft_metadata['image'];
-                        obj['name'] = `${nft_metadata['name']} #${token_id}`;
+                        obj['name'] = `${nft_metadata['name']}`;
                     } catch (e) {
                         // URL is invalid. Nothing we can do about it...
                         return null;
@@ -317,9 +317,6 @@ const explorer = function (server, options, next) {
                 } else {
                     obj['url'] = token_uri;
                     obj['name'] = `${await contract.name()}`;
-                    if (!obj['name'].includes("#")) {
-                        obj['name'] += ` #${token_id}`;
-                    }
                 }
                 try {
                     new URL(obj['url']);
