0ef546e6e7d051011cd97c1cd6400f5371b1a289
maurinlenglart <maurin.lenglart@gmail.com>
2021-09-22 16:30:01 -0700

    bad condition

--- modules/tfuelstake/index.js
+++ modules/tfuelstake/index.js
@@ -1,7 +1,7 @@
 const Boom = require('@hapi/boom')
 const got = require('got');
 
-const tfuel_stake_host = !process.env.NODE_ENV || process.env.NODE_ENV ? "http://localhost:8002" : "http://135.148.122.167:8002";
+const tfuel_stake_host = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "http://localhost:8002" : "http://135.148.122.167:8002";
 
 const tfuelstake = function (server, options, next) {
     server.route([
