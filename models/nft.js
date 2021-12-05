7aa0f753b2ba1d9282e24d693cc5c895f769db10
maurin.lenglart <momohuri>
2021-12-04 17:21:19 -0800

    fix auction numbers

--- models/nft.js
+++ models/nft.js
@@ -45,7 +45,7 @@ module.exports = function (sequelize, DataTypes) {
                 allowNull: false
             },
             price: {
-                type: DataTypes.FLOAT,
+                type: DataTypes.INTEGER,
                 allowNull: false
             },
         },
