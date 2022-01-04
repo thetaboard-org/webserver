0816747858f5a750080e8647f15797ff94fd7387
maurin.lenglart <momohuri>
2022-01-03 17:44:03 -0800

    chaneg default port for mysql

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const manifest = {
                         sequelize: new Sequelize('thetaboard', secrets.database.user, process.env.MYSQL_PASS, {
                             host: process.env.DB,
                             dialect: 'mariadb',
-                            port: process.env.MYSQL_PORT || 3308,
+                            port: process.env.MYSQL_PORT || 3306,
                             dialectOptions: {
                                 options: {
                                     requestTimeout: 3000
