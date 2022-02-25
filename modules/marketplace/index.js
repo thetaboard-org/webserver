df1c40cf4306fa9e7428a6465f06be06abfc2fb4
maurin.lenglart <momohuri>
2022-02-25 10:53:41 -0800

    Drops added to search ui

--- modules/marketplace/index.js
+++ modules/marketplace/index.js
@@ -65,11 +65,12 @@ const marketplace = async function (server, options, next) {
                     const showPerPage = 20;
                     const mktIdx = marketplaceIndex;
 
-                    const tags = [...mktIdx.facetsParams.map((facetName) => {
+                    const tags = mktIdx.facetsParams.map((facetName) => {
                         return req.query[facetName] ? req.query[facetName].split(',').map((x) => `${facetName}:${x}`) : [];
-                    })].filter((x) => !x);
+                    }).flat();
 
                     const searchResults = mktIdx.search(search, {tag: tags, limit: 500});
+                    // all
                     const searchResultsFlat = searchResults.reduce((acc, val) => acc.concat(val.result), []);
                     const searchResultsUnique = [...new Set(searchResultsFlat)];
 
