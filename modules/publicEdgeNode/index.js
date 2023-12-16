b6c5262d65e1322b142eee7a55178db10a2f058a
momohuri <momohuri>
2023-12-15 19:07:39 -0800

    new server for tfuels

--- modules/publicEdgeNode/index.js
+++ modules/publicEdgeNode/index.js
@@ -1,6 +1,6 @@
 const Boom = require('@hapi/boom')
 const got = require('got');
-const tfuel_stake_host = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "http://localhost:8002" : "http://51.81.155.154:8002";
+const tfuel_stake_host = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? "http://localhost:8002" : "http://147.135.65.57:8002";
 
 const MAX_PUBLIC = 10;
 const MAX_AFFILIATE = 5;
